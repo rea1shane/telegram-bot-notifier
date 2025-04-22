98b83e077aabe291e321e6e59bf8c9b821fe4576
Shane <develop@shenyingnan.com>
2025-04-22 04:58:11 -0500

    Update IP API (#2)

--- util/ip/info_test.go
+++ util/ip/info_test.go
@@ -6,7 +6,15 @@ import (
 )
 
 func TestGet(t *testing.T) {
-	info, err := Get()
+	ip, err := Get()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(ip)
+}
+
+func TestQuery(t *testing.T) {
+	info, err := Query("1.1.1.1")
 	if err != nil {
 		panic(err)
 	}
