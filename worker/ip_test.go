40144b105fcbfe4a483d3ccf3adcf0c5019339a6
Shane <develop@shenyingnan.com>
2025-06-20 11:19:16 +0800

    Tiny update

--- worker/ip_test.go
+++ worker/ip_test.go
@@ -15,11 +15,11 @@ func TestIpWatcher_Start(t *testing.T) {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	worker, err := newIPWatcher(logger.With("worker", "IP Watcher"), botToken, chatId)
 	if err != nil {
-		panic(err)
+		t.Fatalf("failed to new ip worker: %v", err)
 	}
 	err = worker.Start()
 	if err != nil {
-		panic(err)
+		t.Fatalf("failed to start ip worker: %v", err)
 	}
 	select {}
 }
